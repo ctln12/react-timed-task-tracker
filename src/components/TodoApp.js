374eed7a5f0fff5125e8399cdc9a56728ffe1e84
Cataline <catalinebaert@yahoo.com>
2021-09-08 18:16:02 +0200

    Remove unused useState import to fix Netlify build fail

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Grid, Paper } from '@material-ui/core';
 import TodoList from './TodoList';
 import useTodoState from '../hooks/useTodoState';
