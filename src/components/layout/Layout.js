e636835af078086e07d55408d8fc9e96eb302bd0
Cataline <catalinebaert@yahoo.com>
2021-09-09 15:45:52 +0200

    Add responsive grid to layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import Navbar from '../Navbar';
 
 const Layout =({children}) =>{
-    return(
-        <>
-          <Navbar/>
-          <main>{children}</main>
-        </>
-    )
+  return(
+    <>
+      <Navbar/>
+      <main>
+        <Grid container justifyContent='center'>
+          <Grid item xs={11} sm={8} md={6} lg={4}>
+            {children}
+          </Grid>
+        </Grid>
+      </main>
+    </>
+  )
 }
 
 export default Layout;
