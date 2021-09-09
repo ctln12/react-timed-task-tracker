a6b54dcaee28c496ec22d7bf2a0c75d4989c7a2a
Cataline <catalinebaert@yahoo.com>
2021-09-09 12:12:57 +0200

    Add home (timer), timer settings and all tasks routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import TodoApp from './components/TodoApp';
 
 function App() {
-  return <TodoApp />;
+  return (
+    <Switch >
+      <Route exact path="/" render={() => <h1>Timer</h1>} />
+      <Route exact path="/settings" render={() => <h1>Timer Settings</h1>} />
+      <Route exact path="/tasks" render={() => <TodoApp />} />
+    </Switch>
+  )
 }
 
 export default App;
