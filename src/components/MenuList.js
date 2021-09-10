d8f5bbf5b902266d86379f41fce6cdafe08ef0ef
Cataline <catalinebaert@yahoo.com>
2021-09-10 22:41:06 +0200

    Add MenuList component to Navbar for navigation

---
+++ src/components/MenuList.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { IconButton, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Close, FormatListBulleted, Settings, Timer } from '@material-ui/icons';
+
+const menu = [
+  {
+    icon: <Timer />,
+    text: 'Timer',
+    path: '/',
+  },
+  {
+    icon: <FormatListBulleted />,
+    text: 'All Tasks',
+    path: '/tasks',
+  },{
+    icon: <Settings />,
+    text: 'Settings',
+    path: '/settings',
+  },
+]
+
+const useStyles = makeStyles((theme) => ({
+  list: {
+    width: '250px',
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  menuLink: {
+    textDecoration: 'none',
+  },
+  item: {
+    padding: theme.spacing(1, 4),
+  },
+}));
+
+function MenuList({toggleDrawer}) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.list}>
+      <div className={classes.drawerHeader}>
+        <IconButton onClick={toggleDrawer(false)}>
+          <Close />
+        </IconButton>
+      </div>
+      <List>
+        {menu.map((item) => (
+          <Link to={item.path} className={classes.menuLink}>
+            <ListItem button key={item.text} className={classes.item}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default MenuList;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Grid, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Grid, IconButton, SwipeableDrawer, Toolbar, Typography } from '@material-ui/core';
 import { Menu, Settings } from '@material-ui/icons';
+import MenuList from './MenuList';
 import TodoList from './TodoList';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,8 +21,16 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ButtonAppBar() {
+function Navbar() {
   const classes = useStyles();
+  const [state, setState] = useState(false);
+  const toggleDrawer = (open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState(open);
+  };
 
   return (
     <div className={classes.root}>
@@ -29,9 +38,17 @@ export default function ButtonAppBar() {
         <Grid container justifyContent='center'>
           <Grid item xs={11} sm={8} md={6} lg={4}>
             <Toolbar>
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)} className={classes.menuButton}>
                 <Menu />
               </IconButton>
+              <SwipeableDrawer
+                anchor="left"
+                open={state}
+                onClose={toggleDrawer(false)}
+                onOpen={toggleDrawer(true)}
+              >
+                <MenuList toggleDrawer={toggleDrawer} />
+              </SwipeableDrawer>
               <Typography variant="h6" className={classes.title}>
                 <Switch >
                   <Route exact path="/" render={() => <h1>Timer</h1>} children={() => 'Timer'} />
@@ -51,3 +68,5 @@ export default function ButtonAppBar() {
     </div>
   );
 }
+
+export default Navbar;
