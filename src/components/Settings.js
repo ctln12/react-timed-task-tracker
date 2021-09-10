9ff2dab230deac7e461549e51345908ce7f24ae0
Cataline <catalinebaert@yahoo.com>
2021-09-10 20:58:57 +0200

    Refactor Settings component and delete unnecessary SelectTime component

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,47 +1,32 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Button, MenuItem, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import SelectTime from './SelectTime';
 import { Link } from 'react-router-dom';
+import useInputState from '../hooks/useInputState';
 
 const numbers = Array.from({length: 120}, (_, i) => i + 1);
 const minutes = numbers.map(number => ({
   value: `${number}`,
-  label: `${number} minutes`
+  label: number === 1 ? `${number} minute` : `${number} minutes`
 }));
-const sessions = [
-  {
-    id: 'focus-session-length',
-    label: 'Focus Session Length',
-    ariaLabel: 'Select focus session length'
-  },
-  {
-    id: 'short-break-length',
-    label: 'Short Break Length',
-    ariaLabel: 'Select short break length'
-  },
-  {
-    id: 'long-break-length',
-    label: 'Long Break Length',
-    ariaLabel: 'Select long break length'
-  },
-  {
-    id: 'long-break-after',
-    label: 'Long Break After',
-    ariaLabel: 'Select long break after'
-  },
-]
+const sessions = numbers.map(number => ({
+  value: `${number}`,
+  label: number === 1 ? `${number} session` : `${number} sessions`
+}));
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: 'fit-content',
-    margin: theme.spacing(2),
+    margin: theme.spacing(3),
   },
-  buttons: {
+  select: {
     margin: theme.spacing(2, 0),
   },
+  buttons: {
+    marginTop: theme.spacing(3),
+  },
   button: {
-    margin: theme.spacing(1, 1),
+    marginBottom: theme.spacing(2),
     width: '100%',
   },
   link: {
@@ -51,13 +36,92 @@ const useStyles = makeStyles((theme) => ({
 
 function Settings() {
   const classes = useStyles();
+  const initialSettings = {
+    focus: '25',
+    shortBreak: '5',
+    longBreak: '15',
+    nbSessions: '4'
+  };
+  const [settings, setSettings] = useState(initialSettings);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSettings(settings);
+    console.log(event.target.value);
+  };
+
+  const [focus, handleFocusChange] = useInputState(settings.focus);
+  const [shortBreak, handleShortBreakChange] = useInputState(settings.shortBreak);
+  const [longBreak, handleLongBreakChange] = useInputState(settings.longBreak);
+  const [nbSessions, handleNbSessionsChange] = useInputState(settings.nbSessions);
+
   return (
-    <form className={classes.root}>
-      {sessions.map(session =>
-        <SelectTime key={session.id} minutes={minutes} session={session} />
-      )}
+    <form className={classes.root} onSubmit={handleSubmit}>
+      <TextField
+        id="focus-session-length"
+        label="Focus Session Length"
+        variant="outlined"
+        select
+        fullWidth
+        value={focus}
+        onChange={handleFocusChange}
+        className={classes.select}
+      >
+        {minutes.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+      <TextField
+        id="short-break-length"
+        label="Short Break Length"
+        variant="outlined"
+        select
+        fullWidth
+        value={shortBreak}
+        onChange={handleShortBreakChange}
+        className={classes.select}
+      >
+        {minutes.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+      <TextField
+        id="long-break-length"
+        label="Long Break Length"
+        variant="outlined"
+        select
+        fullWidth
+        value={longBreak}
+        onChange={handleLongBreakChange}
+        className={classes.select}
+      >
+        {minutes.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+      <TextField
+        id="long-break-after"
+        label="Long Break After"
+        variant="outlined"
+        select
+        fullWidth
+        value={nbSessions}
+        onChange={handleNbSessionsChange}
+        className={classes.select}
+      >
+        {sessions.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
       <div className={classes.buttons}>
-        <Button variant="contained" size="large" className={classes.button}>Save</Button>
+        <Button type="submit" variant="contained" size="large" className={classes.button}>Save</Button>
         <Link to="/" className={classes.link}>
           <Button variant="outlined" size="large" className={classes.button}>Back to timer</Button>
         </Link>
