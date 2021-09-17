532d725f7a9ead167951342feb978ad3d3615a45
Cataline <catalinebaert@yahoo.com>
2021-09-17 18:15:08 +0200

    Create constant base url to clean up code

--- src/hooks/useTodoState.js
+++ src/hooks/useTodoState.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import axios from 'axios';
 
+const baseUrl = 'https://rails-timed-task-tracker-api.herokuapp.com/api/v1/tasks';
+
 const useTodoState = initialTodos => {
   const [todos, setTodos] = useState(initialTodos);
   const addTodo = newTask => {
@@ -8,21 +10,21 @@ const useTodoState = initialTodos => {
       return;
     }
     async function createTodo(newTask) {
-      const response = await axios.post('https://rails-timed-task-tracker-api.herokuapp.com/api/v1/tasks', { name: newTask });
+      const response = await axios.post(baseUrl, { name: newTask });
       setTodos([...todos, response.data]);
     }
     createTodo(newTask);
   };
   const deleteTodo = todoId => {
     async function destroyTodo(id) {
-      const response = await axios.delete(`https://rails-timed-task-tracker-api.herokuapp.com/api/v1/tasks/${id}`)
+      const response = await axios.delete(`${baseUrl}/${id}`)
       setTodos(response.data);
       }
     destroyTodo(todoId);
   };
   const editTodo = (todoId, editedName, editedCompleted) => {
 		async function updateTodo(id, name, completed) {
-      const response = await axios.put(`https://rails-timed-task-tracker-api.herokuapp.com/api/v1/tasks/${id}`,{ name: name, completed: completed });
+      const response = await axios.put(`${baseUrl}/${id}`,{ name: name, completed: completed });
       setTodos(response.data);
     }
 		updateTodo(todoId, editedName, editedCompleted);
